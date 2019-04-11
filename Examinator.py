8e9cb3e804ad6061751b5fe2a99c4536ca87db1d
Henrique <henrique.braz@acad.pucrs.br>
2019-04-11 17:46:44 -0300

    inicio da implementação do dicionario_valores_somados(pai,terras2,dicionario)

--- Examinator.py
+++ Examinator.py
@@ -1,11 +1,10 @@
 
 #Trabalho algoritimos 2
 #Python 3.7.0
+from math import trunc
 
-arq = open('/home/luis/Documentos/pucrs/alest/TrabAlgo2/casos/CasoDev.txt', 'r')
+arq = open('C:\Algo2\TrabAlgo2\casos\CasoDev.txt', 'r')
 pergaminho = arq.readlines()
-print(pergaminho)
-
 
 def terras(pergaminho):
     """
@@ -36,7 +35,14 @@ def primeiro_pai(sub_pergaminho):
     :param pergaminho: sub_pergaminho a ser lido
     :return: String com o nome do primeiro pai
     """
-    return sub_pergaminho[0][0]
+    lista1 = []
+    lista2 = []
+    for i in range(len(sub_pergaminho)):
+        lista1.append(sub_pergaminho[i][1]) #extraindo filhos
+        lista2.append(sub_pergaminho[i][0]) #extraindo pais
+    lista_final = list(set(lista2) - set(lista1)) #retirando o primeiro pai
+    return lista_final[0]
+
 
 def contagem(pai,sub_pergaminho):
     cont = 0
@@ -46,22 +52,19 @@ def contagem(pai,sub_pergaminho):
     return cont
 
 
-def dic_organizator(sub_pergaminho,pai2):
+def dic_organizator(sub_pergaminho,pai):
     dicionario = {}
-    pai = pai2
     count = 0
-    for i in range(15):
+    for i in range(len(sub_pergaminho)):
         if pai == sub_pergaminho[i][0] and count == 0:
             lista = []
             dicionario[sub_pergaminho[i][0]] = lista
             dicionario[sub_pergaminho[i][0]].append(sub_pergaminho[i][1])
             dicionario[sub_pergaminho[i][0]].append(sub_pergaminho[i][2])
             pai = sub_pergaminho[i][0]
-            print(pai+" do if")
             count = 1
 
         elif pai != sub_pergaminho[i][0]:
-            print(pai + " do elif")
             if sub_pergaminho[i][0] in dicionario:
                 dicionario[sub_pergaminho[i][0]].append(sub_pergaminho[i][1])
                 dicionario[sub_pergaminho[i][0]].append(sub_pergaminho[i][2])
@@ -78,9 +81,34 @@ def dic_organizator(sub_pergaminho,pai2):
                 dicionario[pai].append(sub_pergaminho[i][1])
                 dicionario[pai].append(sub_pergaminho[i][2])
                 pai = sub_pergaminho[i][0]
-                print(pai +" do else")
 
     return dicionario
+
+
+def dicionario_valores_somados(pai,terras2,dicionario):
+    terras = terras2
+    lista_pais = list(dicionario.keys())
+    lista_filhos = list(dicionario.values())
+    print('inicio:')
+    print(lista_pais)
+    print(lista_filhos)
+    x = 1
+    contagem = trunc(len(lista_filhos[0])/2)
+    valor_para_somar = trunc((int(terras) / int(contagem)))
+    for k, v in dicionario.items():
+        print(k)
+        if k == lista_pais[0]:
+            valor = int(v[x])
+            valor = valor + valor_para_somar
+            v[x] = str(valor)
+            x += 2
+            print('oi'+ str(x))
+
+
+    print(dicionario)
+
+
+
 
 
 
@@ -95,12 +123,15 @@ terras = terras(pergaminho)
 #extraindo a contagem para a divisão de terras do primeiro pai
 contagem = (contagem(pai,sub_pergaminho))
 
+#extraindo o dicionario:
+dicionario = (dic_organizator(sub_pergaminho,pai))
+
 
 print(pai)
 print(terras)
-print(sub_pergaminho)
 print(contagem)
+print(sub_pergaminho)
+print(dicionario)
 
-print(dic_organizator(sub_pergaminho,pai))
-
+dicionario_valores_somados(pai,terras,dicionario)
 
