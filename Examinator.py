69d94944a78db9aaa473a446ebecc10567def076
Henrique <henrique.braz@acad.pucrs.br>
2019-04-13 22:41:00 -0300

    função dicionario_valores concluída

--- Examinator.py
+++ Examinator.py
@@ -62,7 +62,7 @@ def dic_organizator(sub_pergaminho,pai):
     """
     Função que retorna um dicionario ordenado por ordem de inserção
     :param pai: nome do primeiro pai
-    :param sub_pergaminho: sub_pergaminho a ser lido
+    :param sub_pergaminho: lista a ser lida
     :return: class 'collections.OrderedDict
     """
     dicionario = collections.OrderedDict()
@@ -98,39 +98,40 @@ def dic_organizator(sub_pergaminho,pai):
     return dicionario
 
 
-def dicionario_valores_somados(pai,terras2,dicionario):
-    """
-    Função que retorna um OrderedDict com os valores das terras dos filhos somados
-    :param pai: nome do primeiro pai
-    :param dicionario: OrderedDict a ser lido
-    :return: class 'collections.OrderedDict
-    """
-    x = 0
-    y = 0
-    terras = terras2
-    lista_pais = list(dicionario.keys())
-    lista_filhos = list(dicionario.values())
-    print('***** inicio: ******')
-    print(lista_pais)
-    print(lista_filhos)
-    for i in dicionario.keys():
-        contagem = trunc(len(lista_filhos[x])/2)
-        valor_para_somar = trunc((int(terras) / int(contagem)))
-        value = len(dicionario[lista_pais[y]])
-        for i in range(1,value,2):
-            valor = int(dicionario[lista_pais[y]][i])
-            valor = valor + valor_para_somar
-            dicionario[lista_pais[y]][i] = str(valor)
-            terras = 0
-            x += 1
-            y += 1
-            print('*')
-            
-
-    print(dicionario)
-    print(valor_para_somar)
-
-
+def dicionario_valores(pai,terras,dicionario):
+   """
+   Função que retorna um OrderedDict com os valores das terras dos filhos somados
+   :param pai: nome do primeiro pai
+   :param terras: a quantidade de terras do primeiro pai
+   :param dicionario: OrderedDict a ser lido
+   :return: class 'collections.OrderedDict
+   """
+   y = 0
+   x = 1
+   count = 0
+   lista_pais = list(dicionario.keys())
+   lista_filhos = list(dicionario.values())
+   for i in range(len(lista_pais)):
+      contagem = trunc(len(lista_filhos[i])/2)
+      if count == 0:
+          terras = terras
+          count  = 1
+      else:
+          pos = (lista_filhos[0].index(lista_pais[x])) #pegando a posição do nome do pai
+          terras = dicionario[lista_pais[0]][pos +1] #pegando posição do dicionario
+          x += 1
+         
+      valor_para_somar = trunc((int(terras) / int(contagem)))
+      value = len(lista_filhos[i])
+      for i in range(1,value,2):
+          valor = int(dicionario[lista_pais[y]][i])
+          valor = valor + valor_para_somar
+          dicionario[lista_pais[y]][i] = str(valor)
+          terras = 0
+      y += 1
+       
+   return dicionario 
+     
 
 #extraindo uma listas com sublistas do pergaminho:
 sub_pergaminho = sublistas(pergaminho)
@@ -147,17 +148,19 @@ dicionario = (dic_organizator(sub_pergaminho,pai))
 
 
 #dicionario pronto:
-dicionario_valores_somados(pai,terras,dicionario)
+dicionario = dicionario_valores(pai,terras,dicionario)
 
-
+print('primeiro pai:')
 print(pai)
 print('\n')
+print('quantidade de terras do primeiro pai:')
 print(terras)
 print('\n')
+print('contagem dos filhos do primeiro pai:')
 print(contagem)
-print('\n')
+print('\nsub_pergaminho')
 print(sub_pergaminho)
-print('\n')
+print('\ndicionario')
 print(dicionario)
 
 
